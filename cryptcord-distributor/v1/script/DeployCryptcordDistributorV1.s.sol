7ad489b138ca4dc4cac1041289ae67289ab55e4d
mustafazeydani <mustafamari20@gmail.com>
2024-10-02 23:28:23 +0300

    Added initial setup for the deployment script

--- cryptcord-distributor/v1/script/DeployCryptcordDistributorV1.s.sol
+++ cryptcord-distributor/v1/script/DeployCryptcordDistributorV1.s.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {CryptcordDistributorV1} from "../src/CryptcordDistributorV1.sol";
 
 contract DeployCryptcordDistributorV1 is Script {
-    
+    function run() external returns (CryptcordDistributorV1) {
+        vm.startBroadcast();
+        CryptcordDistributorV1 cryptcordDistributorV1 = new CryptcordDistributorV1();
+        vm.stopBroadcast();
+        return cryptcordDistributorV1;
+    }
 }
