d6288ed15d4a51cf6b3884f5d565ec3f52c97a6f
mustafazeydani <mustafamari20@gmail.com>
2024-10-05 08:39:38 +0300

    refactor: improved the variables' naming and gas optimization

--- cryptcord-distributor/v1/src/CryptcordDistributorV1.sol
+++ cryptcord-distributor/v1/src/CryptcordDistributorV1.sol
@@ -3,80 +3,54 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+// Errors
+error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+// Events
+event TokensTransferred(
+    address indexed erc20,
+    address indexed from,
+    address indexed to,
+    uint256 amount,
+    uint256 fee,
+    uint256 transferAmount,
+    bytes16 paymentId
+);
 
 contract CryptcordDistributorV1 is Ownable {
     constructor() Ownable(msg.sender) {}
 
-    uint256 scaleFactor = 1000;
-    uint256 feePercentage = 75;
+    using SafeERC20 for IERC20;
 
-    function setFeePercentage(uint256 _feePercentage) external onlyOwner {
-        feePercentage = _feePercentage;
+    uint256 constant SCALE_FACTOR = 1000;
+    uint256 s_feePercentage = 75;
+
+    function setFeePercentage(uint256 feePercentage) external onlyOwner {
+        s_feePercentage = feePercentage;
     }
 
-    // Errors
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-    error TransferFailed(
-        address tokenAddress,
-        address from,
-        address to,
-        uint256 value
-    );
-
-    // Events
-    event Transfer(
-        address indexed erc20,
-        address indexed from,
-        address indexed to,
-        uint256 amount,
-        uint256 fee,
-        uint256 transferAmount,
-        bytes16 paymentId
-    );
-
-    function transferTokens(
-        address erc20,
-        uint256 amount,
-        address from,
-        address to,
-        bytes16 paymentId
-    ) public {
+    function transferTokens(address erc20, uint256 amount, address from, address to, bytes16 paymentId) public {
         IERC20 token = IERC20(erc20);
-        uint256 fromBalance = token.balanceOf(from);
+        uint256 senderBalance = token.balanceOf(from);
 
         // Check if sender has enough balance
-        if (fromBalance < amount) {
-            revert InsufficientBalance({
-                balance: fromBalance,
-                withdrawAmount: amount
-            });
+        if (senderBalance < amount) {
+            revert InsufficientBalance({balance: senderBalance, withdrawAmount: amount});
         }
 
         // Calculate the amounts
-        uint256 fee = (amount * feePercentage) / scaleFactor;
+        uint256 fee = (amount * s_feePercentage) / SCALE_FACTOR;
         uint256 transferAmount = amount - fee;
 
         // Transfer the fee to the owner
-        if (!token.transferFrom(from, owner(), fee)) {
-            revert TransferFailed({
-                tokenAddress: erc20,
-                from: from,
-                to: owner(),
-                value: fee
-            });
-        }
+        token.safeTransferFrom(from, owner(), fee);
 
         // Transfer the remaining amount to the destination address
-        if (!token.transferFrom(from, to, transferAmount)) {
-            revert TransferFailed({
-                tokenAddress: erc20,
-                from: from,
-                to: to,
-                value: transferAmount
-            });
-        }
+        token.safeTransferFrom(from, to, transferAmount);
 
         // Event
-        emit Transfer(erc20, from, to, amount, fee, transferAmount, paymentId);
+        emit TokensTransferred(erc20, from, to, amount, fee, transferAmount, paymentId);
     }
 }
